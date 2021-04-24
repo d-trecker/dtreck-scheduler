8956371086c2c2b84d8bd181736ba399eca403c0
d trec <dylan.j.trecker@gmail.com>
2021-04-24 01:44:31 -0700

    Added time block bootstrap and moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
+const apt = document.querySelectorAll("#table-edit td.apt");
 
+// const apt = document.getElementById("aptId");
+const timeRow = document.getElementById("time-row");
+
+apt.forEach(function (aptSet) {
+  aptSet.addEventListener("click", function () {
+    apt.style.background = "blue";
+  });
+});
+
+// var t = document.getElementById("table-edit"),
+//   d = t.getElementByTagName("tr")[0],
+//   r = d.getElementByTagName("td")[0];
+
+// apt.onclick = function () {
+//   debugger;
+//   apt.style.background = "blue";
+// };
+
+//save to local storage
+
+// save appointment
+
+//Change class color based on hour
