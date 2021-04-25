f1fa5b4727a68db9b3707a3771605566bf07d592
d trec <dylan.j.trecker@gmail.com>
2021-04-25 15:35:09 -0700

    Added save button to save to local storage and events will presist on page reload

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Title: Work Day Scheduler </br>
 Student: Dylan Trecker</br>
-Description: A calendar application.</br>
-Site Link: </br>
+Description: A work day scheduler.</br>
+Site Link: https://d-trecker.github.io/dtreck-scheduler/ </br>
 Full Repository: https://github.com/d-trecker/dtreck-scheduler.git </br>
 Get the repository by: $ git clone git@github.com:d-trecker/dtreck-scheduler.git </br>
 
@@ -22,7 +22,7 @@ THEN the current day is displayed at the top of the calendar
 WHEN I scroll down
 THEN I am presented with time blocks for standard business hours
 
--
+-Added time blocks for standard buisness hours.
 
 WHEN I view the time blocks for that day
 THEN each time block is color-coded to indicate whether it is in the past, present, or future
@@ -32,17 +32,17 @@ THEN each time block is color-coded to indicate whether it is in the past, prese
 WHEN I click into a time block
 THEN I can enter an event
 
--
+-Able to enter event on time block.
 
 WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
 
--
+-Event is saved to local storage once save button is clicked.
 
 WHEN I refresh the page
 THEN the saved events persist
 
--
+-If page is refreshed, saved events persist.
 
 </br>
 </br>
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,23 +11,58 @@ setInterval(timeUpdate, 1000);
 timeUpdate();
 //----End Display Time----
 
-//----Capture User input----
-var apptInput = function () {
-  $(".edit").on("click");
-  console.log(apptInput);
+//----Save Tasks to Local Storage----
+var saveAppt = function (key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
 };
+//----Load Tasks from Local Storage when re-opening or refreshing page----
+window.onload = function () {
+  document.getElementById("appt9").textContent =
+    document.getElementById("appt9").value === ""
+      ? localStorage.getItem("9AM").replace(/"/g, "")
+      : "";
 
-//----End Capture User Input----
+  document.getElementById("appt10").textContent =
+    document.getElementById("appt10").value === ""
+      ? localStorage.getItem("10AM").replace(/"/g, "")
+      : "";
 
-//----Save Tasks----
+  document.getElementById("appt11").textContent =
+    document.getElementById("appt11").value === ""
+      ? localStorage.getItem("11AM").replace(/"/g, "")
+      : "";
 
-var saveAppt = function () {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  document.getElementById("appt12").textContent =
+    document.getElementById("appt12").value === ""
+      ? localStorage.getItem("12PM").replace(/"/g, "")
+      : "";
+
+  document.getElementById("appt1").textContent =
+    document.getElementById("appt1").value === ""
+      ? localStorage.getItem("1PM").replace(/"/g, "")
+      : "";
+
+  document.getElementById("appt2").textContent =
+    document.getElementById("appt2").value === ""
+      ? localStorage.getItem("2PM").replace(/"/g, "")
+      : "";
+
+  document.getElementById("appt3").textContent =
+    document.getElementById("appt3").value === ""
+      ? localStorage.getItem("3PM").replace(/"/g, "")
+      : "";
+
+  document.getElementById("appt4").textContent =
+    document.getElementById("appt4").value === ""
+      ? localStorage.getItem("4PM").replace(/"/g, "")
+      : "";
+
+  document.getElementById("appt5").textContent =
+    document.getElementById("appt5").value === ""
+      ? localStorage.getItem("5PM").replace(/"/g, "")
+      : "";
 };
 
 //--------------Things still needed to do--------------
-//save to local storage
-
-// save appointment
 
 //Change class color based on hour
